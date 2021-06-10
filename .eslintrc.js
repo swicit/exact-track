cb7904db1c7cd16e3158bced73fd56fc8b16c0df
Manav Misra <manavm@visionwebsoft.com>
2021-06-09 19:59:29 -0500

    Replace 'airbnb' with 'xo'

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,10 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: ["xo", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  rules: {
-    "array-callback-return": "warn",
-    "import/extensions": "warn",
-    "import/prefer-default-export": "warn",
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    "no-unused-expressions": ["error", { allowShortCircuit: true }],
-    "no-unused-vars": "warn",
-  },
+  rules: {},
 };
