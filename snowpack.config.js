1bf5681f0da2073c5a853bbedb09b0b0369c1e76
Manav Misra <manavm@visionwebsoft.com>
2021-06-23 18:15:07 -0500

    :wrench: Add Supabase URL and 🔑
    Co-authored-by: efoejean <efoejean@users.noreply.github.com>

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,9 +7,6 @@ module.exports = {
     /* ... */
   },
   plugins: ["@snowpack/plugin-postcss"],
-  packageOptions: {
-    /* ... */
-  },
   devOptions: {
     /* ... */
   },
@@ -17,4 +14,9 @@ module.exports = {
     /* ... */
   },
   root: "./src",
+  env: {
+    API_URL: "https://zrxyigghnyercwzabogh.supabase.co",
+    API_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMzI4MDQ4MywiZXhwIjoxOTM4ODU2NDgzfQ.P2WctZhF65KQRKFF6zqUAK_nk9-QjNGndHS2oehaPe8",
+  },
 };
