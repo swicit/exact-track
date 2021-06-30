65f4ae0bcbf9420de585b06d1a4be78e8a84c05e
Manav Misra <manavm@visionwebsoft.com>
2021-06-30 18:49:01 -0500

    :goal_net: Show error alert
    Co-authored-by: efoejean <efoejean@users.noreply.github.com>
    
    :goal_net: Show and hide the error message

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,30 @@
 import "alpinejs";
 import { registerUser } from "./services";
 
-console.log(registerUser);
+const errorSection = document.getElementById("error");
+const errorSectionP = errorSection.querySelector("p");
+
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener("focus", () => {
+    // Add the hidden class to hide the error
+    errorSection.classList.add("hidden");
+  });
+});
 
 document.querySelector("form").addEventListener("submit", (ev) => {
   ev.preventDefault();
-  const newRegistration = Object.fromEntries(new FormData(ev.target));
+  const newUser = Object.fromEntries(new FormData(ev.target));
+
+  registerUser(newUser)
+    .then(({ user, error: sbError }) => {
+      if (sbError) {
+        throw Error(sbError.message);
+      }
+    })
+    .catch((customError) => {
+      errorSectionP.innerText = customError.message;
+
+      // Remove 'hidden' class to show the error
+      errorSection.classList.remove("hidden");
+    });
 });
